e3caea87a15553fc8f8cab59d582908a699d493b
Mateo521 <mateovillalva42@gmail.com>
2026-08-09 19:49:24 -0300

    ajustes

--- resources/js/Pages/Gallery/Show.vue
+++ resources/js/Pages/Gallery/Show.vue
@@ -122,9 +122,7 @@ const handleImageError = (e) => {
                                 @error="handleImageError" />
                             
                         
-                            <div class="absolute bottom-8 left-8 bg-black/60 backdrop-blur-md text-white px-4 py-2 rounded-full text-xs font-bold tracking-wider pointer-events-none">
-                                Protección Activa
-                            </div>
+                            
                         </div>
                     </div>
 
