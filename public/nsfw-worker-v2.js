6d75a58919aff4fafcf45c1a0d7124d04b9e0ae8
Mateo521 <mateovillalva42@gmail.com>
2026-09-10 20:04:29 -0300

    nsfw v2

--- public/nsfw-worker-v2.js
+++ public/nsfw-worker-v2.js
@@ -3,7 +3,7 @@ importScripts('https://unpkg.com/nsfwjs');
 
 let model = null;
 
-nsfwjs.load('https://unpkg.com/nsfwjs/model/').then(loadedModel => {
+nsfwjs.load().then(loadedModel => {
     model = loadedModel;
     postMessage({ status: 'READY' });
 }).catch(err => {
