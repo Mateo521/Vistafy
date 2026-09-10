1683ab571cc8c7f2e79f4fab94f41795c2082795
Mateo521 <mateovillalva42@gmail.com>
2026-09-10 20:13:40 -0300

    nsfw v4

--- public/nsfw-worker-v4.js
+++ public/nsfw-worker-v4.js
@@ -3,27 +3,26 @@ importScripts('https://unpkg.com/nsfwjs');
 
 let model = null;
 
-nsfwjs.load('https://unpkg.com/nsfwjs@4.4.0/model/').then(loadedModel => {
+
+nsfwjs.load('https://s3.amazonaws.com/ir_public/nsfwj/v1/').then(loadedModel => {
     model = loadedModel;
     postMessage({ status: 'READY' });
 }).catch(err => {
     console.error("Error cargando modelo NSFW en el worker:", err);
 });
+
 self.onmessage = async (e) => {
     if (!model) return;
     
-
     const { fileId, file } = e.data;
 
     try {
-
         const bmp = await createImageBitmap(file);
         const canvas = new OffscreenCanvas(bmp.width, bmp.height);
         const ctx = canvas.getContext('2d');
         ctx.drawImage(bmp, 0, 0);
         const imageData = ctx.getImageData(0, 0, bmp.width, bmp.height);
         
-
         const predictions = await model.classify(imageData);
         
 
@@ -31,12 +30,10 @@ self.onmessage = async (e) => {
             (p.className === 'Porn' || p.className === 'Hentai') && p.probability > 0.60
         );
 
-
         postMessage({ fileId, isSafe: !isUnsafe });
         bmp.close();
         
     } catch (error) {
-
         postMessage({ fileId, isSafe: true });
     }
 };
--- resources/js/Pages/Photographer/Photos/Create.vue
+++ resources/js/Pages/Photographer/Photos/Create.vue
@@ -19,7 +19,7 @@ import Tesseract from 'tesseract.js';
 
 
 
-const nsfwWorker = new Worker('/nsfw-worker-v3.js');
+const nsfwWorker = new Worker('/nsfw-worker-v4.js');
 let isNsfwReady = false;
 const isScanningNSFW = ref(false);  
 
